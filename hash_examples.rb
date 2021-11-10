f6606ebe3173f6bf2ba9449d617d5c3b76c18ce6
Joseph Downs <jos.w.downs@gmail.com>
2021-11-09 20:32:04 -0700

    Add animal hash

--- hash_examples.rb
+++ hash_examples.rb
@@ -1,6 +1,12 @@
 car = {
-  make = "Ford",
-  model = "Fusion",
-  color = "gray"
-  year = "2008"
+  make: "Ford",
+  model: "Fusion",
+  color: "gray",
+  year: "2008"
+}
+
+animal = {
+  family: "feline",
+  species: "lion",
+  color: "yellow"
 }
